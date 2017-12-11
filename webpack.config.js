bcbef8e2f6e2dcce1b20246091aa551733d6efee
frizadiga <frizadiga@gmail.com>
2017-12-11 10:44:35 +0700

    add js main client & rename bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'app.bundle.js'
   },
   module: {
     rules: [{
