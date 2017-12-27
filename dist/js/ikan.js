7eaeffdbcbb5f1c432a84e08d44e25ff72ef8a15
frizadiga <frizadiga@gmail.com>
2017-12-27 09:24:27 +0700

    update

--- dist/js/ikan.js
+++ dist/js/ikan.js
@@ -287,10 +287,7 @@ process.umask = function() { return 0; };
 /* 3 */
 /***/ (function(module, exports) {
 
-var ikan = function ikan(v) {
-  console.log("ikan Framework v." + v);
-};
-module.exports = ikan;
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open 'A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\.babelrc'\n    at Object.fs.openSync (fs.js:663:18)\n    at Object.fs.readFileSync (fs.js:568:33)\n    at ConfigChainBuilder.addConfig (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-core\\lib\\transformation\\file\\options\\build-config-chain.js:146:32)\n    at ConfigChainBuilder.findConfigs (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-core\\lib\\transformation\\file\\options\\build-config-chain.js:96:16)\n    at buildConfigChain (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-core\\lib\\transformation\\file\\options\\build-config-chain.js:61:13)\n    at OptionManager.init (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:354:58)\n    at File.initOptions (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-core\\lib\\transformation\\file\\index.js:212:65)\n    at new File (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-core\\lib\\transformation\\file\\index.js:135:24)\n    at Pipeline.transform (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-core\\lib\\transformation\\pipeline.js:46:16)\n    at transpile (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-loader\\lib\\index.js:50:20)\n    at Object.module.exports (A:\\hiuData\\hiuWEB\\App\\hiu\\ikan\\node_modules\\babel-loader\\lib\\index.js:175:20)");
 
 /***/ })
 /******/ ]);
