48b479b83e0b8a211c685261707d381c3669565d
frizadiga <frizadiga@gmail.com>
2018-01-03 16:13:59 +0700

    add search filter feature & refactor

--- app/assets/html.js
+++ app/assets/html.js
@@ -2,7 +2,7 @@
 // $http = axios;
 
 const ikan = {};
-ikan.includeHTML = function(cb) {
+ikan.includeHTML = (cb) => {
   var z, i, elmnt, file, xhttp;
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
@@ -31,6 +31,39 @@ ikan.includeHTML = function(cb) {
   }
   if (cb) cb();
 };
+
+ikan.getElements = (id) => {
+  if (typeof id == "object") {
+    return [id];
+  } else {
+    return document.querySelectorAll(id);
+  }
+};
+
+ikan.filterHTML = (id, sel, filter) => {
+  var a, b, c, i, ii, iii, hit;
+  a = ikan.getElements(id);
+  for (i = 0; i < a.length; i++) {
+    b = ikan.getElements(sel);
+    for (ii = 0; ii < b.length; ii++) {
+      hit = 0;
+      if (b[ii].innerHTML.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+        hit = 1;
+      }
+      c = b[ii].getElementsByTagName("*");
+      for (iii = 0; iii < c.length; iii++) {
+        if (c[iii].innerHTML.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+          hit = 1;
+        }
+      }
+      if (hit == 1) {
+        b[ii].style.display = "";
+      } else {
+        b[ii].style.display = "none";
+      }
+    }
+  }
+}
 
 function ikanLink(link){
   document.getElementById('content').setAttribute("ikan-include-html", link);
